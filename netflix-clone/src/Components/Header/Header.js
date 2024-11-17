a2ed36d9ac37203ae3dc8cc6b0df96a80a400177
Temesgen Gebereyesu <t.geber@yahoo.com>
2024-11-17 01:04:17 -0500

    headre and footer

---
+++ netflix-clone/src/Components/Header/Header.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./header.css";
+import NetflixLogo from "../../assets/images/NetflixLogo.jpeg";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+  return (
+    <div className="Header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" width="100" />
+            </li>
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <searchIcon />
+            </li>
+            <li>
+              <NotificationNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>{" "}
+            */} /*{" "}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
